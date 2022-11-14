3b3e5b6839f569a4c7b280bfe81e667187b5e4d0
unknown <shwezinoo44422@gmail.com>
2022-11-14 22:33:01 +0630

    test_fourth

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -117,9 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
